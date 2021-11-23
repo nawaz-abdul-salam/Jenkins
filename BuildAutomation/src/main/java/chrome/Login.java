09b4dd4ec6b5c28adf070592dfc37c71996c8f0b
nawaz-abdul-salam <nawasabdulsalam@yahoo.com>
2021-11-23 23:24:10 +0530

    Update Login.java

--- BuildAutomation/src/main/java/chrome/Login.java
+++ BuildAutomation/src/main/java/chrome/Login.java
@@ -40,7 +40,7 @@ public class Login {
 		driver.findElementByClassName("decorativeSubmit").click();
 		driver.findElementByClassName("decorativeSubmit").click();
 		driver.close();
-		driver.quit();
+		
 	}
 
 
